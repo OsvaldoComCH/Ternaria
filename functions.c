32a4c3dc724e40950f2b406a7e067e1c0f35937c
OsvaldoComCH <eduhrsilva@gmail.com>
2024-07-04 16:30:10 -0300

    redução de lag

--- functions.c
+++ functions.c
@@ -20,14 +20,20 @@ void EraseRect(HDC hdc, const RECT * Rect)
 void DrawImg(HDC hdc, const RECT * Rect, const wchar_t * ImgPath)
 {
     BITMAP bm;
-    HBITMAP Image = (HBITMAP)LoadImage(NULL, ImgPath, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    static HBITMAP Image = NULL;
+    static wchar_t * LastImg = NULL;
+    if(ImgPath != LastImg)
+    {
+        DeleteObject(Image);
+        Image = (HBITMAP)LoadImage(NULL, ImgPath, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    }
     HDC BitmapDC = CreateCompatibleDC(hdc);
     SelectObject(BitmapDC, Image);
     GetObject((HGDIOBJ)Image, sizeof(bm), &bm);
     TransparentBlt(hdc, Rect->left, Rect->top, bm.bmWidth, bm.bmHeight, BitmapDC,
     0, 0, Rect->right - Rect->left, Rect->bottom - Rect->top, RGB(255, 0, 255));
     DeleteDC(BitmapDC);
-    DeleteObject(Image);
+    LastImg = ImgPath;
 }
 
 // Desenha retângulo
